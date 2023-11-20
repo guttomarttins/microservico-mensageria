d101f4f636f84f6d2ac1ea3aba5c721de4e11f9d
guttomarttins <guttomarttins@gmail.com>
2023-11-20 13:08:13 -0300

    configurando micrservices client

---
+++ msclient/src/test/java/br/com/gt/msclient/MsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gt.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
